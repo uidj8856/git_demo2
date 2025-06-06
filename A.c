b52bc91549a5c2b76858d2fb6d0587cb94e1b9e6
Sana Sivaprasada (uidj8856) <Sivaprasada.Sana@continental-corporation.com>
2025-06-06 14:08:40 +0530

    A.c with updates

--- A.c
+++ A.c
@@ -11,7 +11,18 @@ void reverseString(char *str) {
         str[j] = temp;
     }
 }
-
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
 int main() {
     char str[100];
     printf("Enter a string: ");
